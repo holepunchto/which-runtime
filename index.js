1727364569e4d0cb7fe7e9dd0972904aac121d0c
Geordan Gesink <georgegeordan@gmail.com>
2025-09-11 16:45:40 +0200

    add isMobile

--- index.js
+++ index.js
@@ -17,11 +17,14 @@ const isExpo = typeof global !== 'undefined' &&
    global.ExpoModules ||
    (typeof global.require !== 'undefined' && global.require.main?.filename?.includes('expo')))
 
+const isMobile = isReactNative || isExpo || isIOS || isAndroid
+
 exports.runtime = runtime
 exports.platform = platform
 exports.arch = arch
 exports.isReactNative = isReactNative
 exports.isExpo = isExpo
+exports.isMobile = isMobile
 exports.isBare = runtime === 'bare'
 exports.isBareKit = exports.isBare && typeof BareKit !== 'undefined'
 exports.isPear = typeof Pear !== 'undefined'
