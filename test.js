96b12a9554bd2b8af5020a71b1b2acf7429bfe62
Mikkel Malmberg <mikkel@brnbw.com>
2026-01-06 12:09:56 +0100

    Add test, linter, CI

---
+++ test.js
@@ -0,0 +1,44 @@
+const test = require('brittle')
+const which = require('.')
+
+test('basic exports', function (t) {
+  t.is(typeof which.runtime, 'string')
+  t.is(typeof which.platform, 'string')
+  t.is(typeof which.arch, 'string')
+  t.is(typeof which.isBare, 'boolean')
+  t.is(typeof which.isBareKit, 'boolean')
+  t.is(typeof which.isPear, 'boolean')
+  t.is(typeof which.isNode, 'boolean')
+  t.is(typeof which.isBrowser, 'boolean')
+  t.is(typeof which.isWindows, 'boolean')
+  t.is(typeof which.isLinux, 'boolean')
+  t.is(typeof which.isMac, 'boolean')
+  t.is(typeof which.isIOS, 'boolean')
+  t.is(typeof which.isAndroid, 'boolean')
+  t.is(typeof which.isElectron, 'boolean')
+  t.is(typeof which.isElectronRenderer, 'boolean')
+  t.is(typeof which.isElectronWorker, 'boolean')
+})
+
+test('runtime detection', function (t) {
+  // eslint-disable-next-line eqeqeq
+  if (typeof Bare != 'undefined') {
+    t.is(which.runtime, 'bare')
+    t.is(which.isBare, true)
+    t.is(which.isNode, false)
+  } else {
+    t.is(which.runtime, 'node')
+    t.is(which.isBare, false)
+    t.is(which.isNode, true)
+  }
+})
+
+test('platform detection', function (t) {
+  const platforms = ['win32', 'linux', 'darwin', 'ios', 'ios-simulator', 'android']
+  t.ok(platforms.includes(which.platform) || which.platform === 'unknown')
+
+  // Only one of these should be true at a time (or none if unknown platform)
+  const platformFlags = [which.isWindows, which.isLinux, which.isMac, which.isIOS, which.isAndroid]
+  const trueCount = platformFlags.filter(Boolean).length
+  t.ok(trueCount <= 1, 'at most one platform flag should be true')
+})
